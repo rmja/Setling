80d05493f52c970f40cd6e73a184e32c9c439d61
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-10-13 09:11:55 +0200

    Add IsoDayOfWeek extension to StartOfUnit

---
+++ src/Setling/IsoDayOfWeekExtensions.cs
@@ -0,0 +1,20 @@
+﻿using NodaTime;
+using System;
+
+namespace Setling
+{
+    public static class IsoDayOfWeekExtensions
+    {
+        public static StartOfUnit ToStartOfUnit(this IsoDayOfWeek dayOfWeek) => dayOfWeek switch
+        {
+            IsoDayOfWeek.Monday => StartOfUnit.Monday,
+            IsoDayOfWeek.Tuesday => StartOfUnit.Tuesday,
+            IsoDayOfWeek.Wednesday => StartOfUnit.Wednesday,
+            IsoDayOfWeek.Thursday => StartOfUnit.Thursday,
+            IsoDayOfWeek.Friday => StartOfUnit.Friday,
+            IsoDayOfWeek.Saturday => StartOfUnit.Saturday,
+            IsoDayOfWeek.Sunday => StartOfUnit.Sunday,
+            _ => throw new ArgumentException()
+        };
+    }
+}
